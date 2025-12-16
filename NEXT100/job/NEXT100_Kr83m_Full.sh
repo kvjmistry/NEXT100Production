3a35203374f3c29670db6303a5b1ae5fb57ce77d
kvjmistry <kvjmistry@gmail.com>
2025-12-16 17:11:55 +0100

    Update job submit for testing branch

--- NEXT100/job/NEXT100_Kr83m_Full.sh
+++ NEXT100/job/NEXT100_Kr83m_Full.sh
@@ -34,12 +34,13 @@ INIT=${JOBNAME}.init.mac
 
 echo "N_EVENTS: ${N_EVENTS}"
 
-SEED=$((${N_EVENTS}*${JOBID} + ${N_EVENTS}))
+EID=$((${N_EVENTS}*${JOBID} + ${N_EVENTS}))
+SEED=$((${JOBID} + 1))
 echo "The seed number is: ${SEED}" 
 
 # Change the config in the files
 sed -i "s#.*random_seed.*#/nexus/random_seed ${SEED}#" ${CONFIG}
-sed -i "s#.*start_id.*#/nexus/persistency/start_id ${SEED}#" ${CONFIG}
+sed -i "s#.*start_id.*#/nexus/persistency/start_id ${EID}#" ${CONFIG}
 
 # Print out the config and init files
 cat ${INIT}
--- NEXT100/job/NEXT100_Tl208_Port1a_Full.sh
+++ NEXT100/job/NEXT100_Tl208_Port1a_Full.sh
@@ -18,26 +18,27 @@ start=`date +%s`
 # Setup nexus
 echo "Setting Up Software" 
 source /software/nexus/setup_nexus.sh
-source /software/IC/setup_IC.sh
+#source /software/IC/setup_IC.sh
 
 echo "untaring files"
 tar -xvf files_${PRESSURE}.tar
 rm files_${PRESSURE}.tar
 
 # Set the configurable variables
-N_EVENTS=10000
+N_EVENTS=300 # will give ~1 event per job. Some jobs 0 events
 CONFIG=${JOBNAME}.config.mac
 INIT=${JOBNAME}.init.mac
 
 
 echo "N_EVENTS: ${N_EVENTS}"
 
-SEED=$((${N_EVENTS}*${JOBID} + ${N_EVENTS}))
+EID=$((${N_EVENTS}*${JOBID} + ${N_EVENTS}))
+SEED=$((${JOBID} + 1))
 echo "The seed number is: ${SEED}" 
 
 # Change the config in the files
 sed -i "s#.*random_seed.*#/nexus/random_seed ${SEED}#" ${CONFIG}
-sed -i "s#.*start_id.*#/nexus/persistency/start_id ${SEED}#" ${CONFIG}
+sed -i "s#.*start_id.*#/nexus/persistency/start_id ${EID}#" ${CONFIG}
 
 # Print out the config and init files
 cat ${INIT}
@@ -47,12 +48,12 @@ cat ${CONFIG}
 echo "Running NEXUS and IC" 
 nexus -n $N_EVENTS ${INIT}
 python compress_nexus.py NEXT100_Tl208_Port1a_Full.h5 NEXT100_Tl208_Port1a_Full_nexus_${JOBID}.h5
-city buffy    buffyTemplate.conf    -i NEXT100_Tl208_Port1a_Full_nexus_${JOBID}.h5    -o NEXT100_Tl208_Port1a_Full_buffy_${JOBID}.h5
-city hypathia hypathiaTemplate.conf -i NEXT100_Tl208_Port1a_Full_buffy_${JOBID}.h5    -o NEXT100_Tl208_Port1a_Full_hypathia_${JOBID}.h5
-city dorothea dorotheaTemplate.conf -i NEXT100_Tl208_Port1a_Full_hypathia_${JOBID}.h5 -o NEXT100_Tl208_Port1a_Full_dorothea_${JOBID}.h5
+# city buffy    buffyTemplate.conf    -i NEXT100_Tl208_Port1a_Full_nexus_${JOBID}.h5    -o NEXT100_Tl208_Port1a_Full_buffy_${JOBID}.h5
+# city hypathia hypathiaTemplate.conf -i NEXT100_Tl208_Port1a_Full_buffy_${JOBID}.h5    -o NEXT100_Tl208_Port1a_Full_hypathia_${JOBID}.h5
+# city dorothea dorotheaTemplate.conf -i NEXT100_Tl208_Port1a_Full_hypathia_${JOBID}.h5 -o NEXT100_Tl208_Port1a_Full_dorothea_${JOBID}.h5
 
 rm NEXT100_Tl208_Port1a_Full.h5
-rm NEXT100_Tl208_Port1a_Full_buffy_${JOBID}.h5
+# rm NEXT100_Tl208_Port1a_Full_buffy_${JOBID}.h5
 rm *LT*
 rm *PSF*
 rm *map*
