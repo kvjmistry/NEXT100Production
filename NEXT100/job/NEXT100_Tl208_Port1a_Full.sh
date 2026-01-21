0eadc26230ffc8627d2d7cb6e32fc21974465f06
kvjmistry <kvjmistry@gmail.com>
2026-01-20 21:55:49 -0600

    Fix IC chain in full sim

--- NEXT100/job/NEXT100_Tl208_Port1a_Full.sh
+++ NEXT100/job/NEXT100_Tl208_Port1a_Full.sh
@@ -70,7 +70,7 @@ done
 python3 compress_nexus.py NEXT100_Tl208_Port1a_Full.h5 NEXT100_Tl208_Port1a_Full_nexus_${JOBID}.h5
 city buffy    buffyTemplate.conf    -i NEXT100_Tl208_Port1a_Full_nexus_${JOBID}.h5    -o NEXT100_Tl208_Port1a_Full_buffy_${JOBID}.h5
 city hypathia hypathiaTemplate.conf -i NEXT100_Tl208_Port1a_Full_buffy_${JOBID}.h5    -o NEXT100_Tl208_Port1a_Full_hypathia_${JOBID}.h5
-city dorothea dorotheaTemplate.conf -i NEXT100_Tl208_Port1a_Full_hypathia_${JOBID}.h5 -o NEXT100_Tl208_Port1a_Full_dorothea_${JOBID}.h5
+city sophronia sophroniaTemplate.conf -i NEXT100_Tl208_Port1a_Full_hypathia_${JOBID}.h5 -o NEXT100_Tl208_Port1a_Full_sophronia_${JOBID}.h5
 
 rm NEXT100_Tl208_Port1a_Full.h5
 # rm NEXT100_Tl208_Port1a_Full_buffy_${JOBID}.h5
