01cc085b70cba95a3fd54a01e86ef77e14a46615
kvjmistry <kvjmistry@gmail.com>
2026-02-12 09:27:13 -0600

    change whether to use DEP or full energy range in job

--- NEXT100/job/NEXT100_Tl208_Port1a.sh
+++ NEXT100/job/NEXT100_Tl208_Port1a.sh
@@ -27,11 +27,27 @@ echo "untaring files"
 tar -xvf files_${PRESSURE}.tar
 rm files_${PRESSURE}.tar
 
-# Set the configurable variables
-N_EVENTS=12000
-CONFIG=${JOBNAME}.config.mac
-INIT=${JOBNAME}.init.mac
+N_EVENTS=10
 
+# Use same template for full energy range and DEP events
+CONFIG=NEXT100_Tl208_Port1a.config.mac
+INIT=NEXT100_Tl208_Port1a.init.mac
+
+# Set the configurable variables
+if [[ "$JOBNAME" == "NEXT100_Tl208_Port1a_DEP" ]]; then
+    echo "Using Double escape peak configurations"
+    N_EVENTS=100000 # for 1.45 - 1.65 DEP MeV ~ 100-150 events per job
+    sed -i "s#.*min_energy.*#/Actions/DefaultEventAction/min_energy 1.45 MeV#" ${CONFIG}
+    sed -i "s#.*max_energy.*#/Actions/DefaultEventAction/max_energy 1.65 MeV#" ${CONFIG}
+else
+    echo "Using full energy spectrum configuration"
+    N_EVENTS=12000 # for 1.00 - 2.80 MeV ~ 100 events per job
+    sed -i "s#.*min_energy.*#/Actions/DefaultEventAction/min_energy 1.0 MeV#" ${CONFIG}
+    sed -i "s#.*max_energy.*#/Actions/DefaultEventAction/max_energy 2.8 MeV#" ${CONFIG}
+fi
+
+# Make sure output file name is consistent
+sed -i "s#.*output_file.*#/nexus/persistency/output_file ${JOBNAME}#" ${CONFIG}
 
 echo "N_EVENTS: ${N_EVENTS}"
 
@@ -51,14 +67,14 @@ cat ${CONFIG}
 # Generation + Reco
 echo "Running NEXUS and IC" 
 nexus -n $N_EVENTS ${INIT}
-python compress_nexus.py NEXT100_Tl208_Port1a.h5 NEXT100_Tl208_Port1a_nexus_${JOBID}.h5
-city detsim    detsimTemplate.conf    -i NEXT100_Tl208_Port1a_nexus_${JOBID}.h5    -o NEXT100_Tl208_Port1a_detsim_${JOBID}.h5
-city hypathia  hypathiaTemplate.conf  -i NEXT100_Tl208_Port1a_detsim_${JOBID}.h5   -o NEXT100_Tl208_Port1a_hypathia_${JOBID}.h5
-city sophronia sophroniaTemplate.conf -i NEXT100_Tl208_Port1a_hypathia_${JOBID}.h5 -o NEXT100_Tl208_Port1a_sophronia_${JOBID}.h5
+python compress_nexus.py ${JOBNAME}.h5 ${JOBNAME}_nexus_${JOBID}.h5
+city detsim    detsimTemplate.conf    -i ${JOBNAME}_nexus_${JOBID}.h5    -o ${JOBNAME}_detsim_${JOBID}.h5
+city hypathia  hypathiaTemplate.conf  -i ${JOBNAME}_detsim_${JOBID}.h5   -o ${JOBNAME}_hypathia_${JOBID}.h5
+city sophronia sophroniaTemplate.conf -i ${JOBNAME}_hypathia_${JOBID}.h5 -o ${JOBNAME}_sophronia_${JOBID}.h5
 
-python GetGammaTables.py NEXT100_Tl208_Port1a_sophronia_${JOBID}.h5 NEXT100_Tl208_Port1a_GammaTables_${JOBID}.h5
+python3 GetGammaTables.py ${JOBNAME}_sophronia_${JOBID}.h5 ${JOBNAME}_GammaTables_${JOBID}.h5
 
-rm NEXT100_Tl208_Port1a.h5
+rm ${JOBNAME}.h5
 rm *LT*
 rm *PSF*
 rm *map*
@@ -66,14 +82,14 @@ rm GetGammaTables.py
 
 # Only keep first 1000 files for validation purposes
 if (( JOBID > 1000 )); then
-    rm NEXT100_Tl208_Port1a_detsim_${JOBID}.h5
-    rm NEXT100_Tl208_Port1a_hypathia_${JOBID}.h5
+    rm ${JOBNAME}_detsim_${JOBID}.h5
+    rm ${JOBNAME}_hypathia_${JOBID}.h5
 fi
 
 ls -ltrh
 
 echo "Taring the h5 files"
-tar -cvf NEXT100_Tl208_Port1a.tar *.h5
+tar -cvf ${JOBNAME}.tar *.h5
 
 # Cleanup
 rm *.h5
